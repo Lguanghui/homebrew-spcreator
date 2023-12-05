769967e537943f3bb113c7c4b0c5b08727c2213f
Guanghui Liang <meetguanghuiliang@gmail.com>
2023-12-05 23:40:37 +0800

    Initial commit.

---
+++ spcreator.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Spcreator < Formula
+  desc "Swift Package creator."
+  homepage "https://github.com/Lguanghui/SPCreator"
+  url "https://github.com/Lguanghui/SPCreator/releases/download/1.2.0/spcreator"
+  sha256 "f96fb3172f5102cb3e0e0b39fae02647d8124d975cb29bfa57abc39dd188ab6b"
+  version "1.2.0"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "spcreator"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test spcreator`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "spcreator --help"
+  end
+end
